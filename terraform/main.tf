ad6cf2494a4e376103e752066f9b2d305487aa56
engzaz <mahmmoud_zakaria@yahoo.com>
2021-03-31 03:06:57 +0000

    before alb config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.34.0"
+    }
+  }
+
+  required_version = "~> 0.14"
+}
+
 provider "aws" {
   profile = var.profile
   alias   = "east"
